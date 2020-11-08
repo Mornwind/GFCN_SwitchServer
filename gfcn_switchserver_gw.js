9a53d4db558ec1d5f83db7ef3887583eb2944856
Mornwind <Mornwind@users.noreply.github.com>
2020-11-09 03:12:19 +0800

    更新：添加Shadowrocket

--- gfcn_switchserver_gw.js
+++ gfcn_switchserver_gw.js
@@ -6,16 +6,16 @@ README:
 https://github.com/Mornwind/GFCN_SwitchServer/blob/master/README.md
 */
 
-let url = $request.url;
+// let url = $request.url;
 let headers = $request.headers;
 let body = $request.body;
 
-url = url.replace(/gfcn-transit\.ios\.sunborngame\.com/,"gfcn-transit.gw.sunborngame.com");
+// url = url.replace(/gfcn-transit\.ios\.sunborngame\.com/,"gfcn-transit.gw.sunborngame.com");
 headers['Host'] = 'gfcn-transit.gw.sunborngame.com';
 body = body.replace(/channel=cn_appstore/,"channel=cn_mica").replace(/device=ios/,"device=adr").replace(/platformChannelId=ios/,"platformChannelId=GWGW");
 
 $done({
-	url: url,
+//	url: url,
 	headers: headers,
 	body: body
 });
