5cc7b21a83f93083a2d543e8193cabfc4d747cf4
Mornwind <Mornwind@users.noreply.github.com>
2020-11-27 21:36:25 +0800

    新增：旧版本小火箭所用配置

---
+++ gfcn_switchserver_gw_old.js
@@ -0,0 +1,21 @@
+/*
+「少女前线」跨服（iOS 端 → 安卓官服）
+By: Mornwind
+
+README:
+https://github.com/Mornwind/GFCN_SwitchServer/blob/master/README.md
+*/
+
+// let url = $request.url;
+let headers = $request.headers;
+let body = $request.body;
+
+// url = url.replace(/gfcn-transit\.ios\.sunborngame\.com/,"gfcn-transit.gw.sunborngame.com");
+headers['Host'] = 'gfcn-transit.gw.sunborngame.com';
+body = body.replace(/channel=cn_appstore/,"channel=cn_mica").replace(/device=ios/,"device=adr").replace(/platformChannelId=ios/,"platformChannelId=GWGW");
+
+$done({
+//	url: url,
+	headers: headers,
+	body: body
+});
