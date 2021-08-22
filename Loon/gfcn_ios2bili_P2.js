c34536c17d05035d230782f23fb9483bb30183db
Mornwind <Mornwind@users.noreply.github.com>
2021-08-23 01:11:48 +0800

    更新：补充遗漏的 Loon 跨 B 服配置及脚本

---
+++ Loon/gfcn_ios2bili_P2.js
@@ -0,0 +1,21 @@
+/*
+「少女前线」跨服（iOS 端 → Bilibili 服）
+By: Mornwind
+
+README:
+https://github.com/Mornwind/GFCN_SwitchServer/blob/master/README.md
+*/
+
+const cookieName = "「少女前线」跨服（iOS 端 → Bilibili 服）";
+const cookie1Key = "gfcn_ios2bili_open_id";
+const cookie1Val = $persistentStore.read(cookie1Key);
+const cookie2Key = "gfcn_ios2bili_token";
+const cookie2Val = $persistentStore.read(cookie2Key);
+
+let body = $request.body;
+
+body = body.replace(/(openid=).*?&/,'$1' + cookie1Val + '&').replace(/(sid=).*?&/,'$1' + cookie2Val + '&');
+
+$done({
+	body: body
+});
